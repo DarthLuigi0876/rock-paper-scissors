19eead97963c454f089eb5cbd409d71a555bdbcf
Ayush <invinciblefoxstar@gmail.com>
2024-12-23 21:59:11 +0530

    Added run round and run game functions

--- logic.js
+++ logic.js
@@ -1,20 +1,74 @@
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
+    let choices = ["rock", "paper", "scissors"];
 
     return choices[Math.floor(Math.random()*3)];
 }
 
 function getUserChoice() {
-    let validChoices=["Rock", "Paper", "Scissors"];
+    let validChoices=["rock", "paper", "scissors"];
     let userChoice = prompt("Select either Rock, Paper or Scissors: ");
+    userChoice = userChoice.toLowerCase();
 
     if(validChoices.indexOf(userChoice) > -1){
         return userChoice;
     } else {
         console.log("Invalid Choice")
+        return 2;
     }
     
 }
 
-console.log(getComputerChoice());
-console.log(getUserChoice());
+function runRound(computerChoice, userChoice){
+    if (computerChoice === userChoice){
+        console.log("This round is a tie")
+        return 2
+    }
+    else if (computerChoice === "rock" && userChoice ==="paper"){
+        return 1
+    }
+    else if (computerChoice === "paper" && userChoice === "scissors"){
+        return 1
+    }
+    else if (computerChoice === "scissors" && userChoice ==="rock"){
+        return 1
+    }
+    else {
+        return 0
+    }
+}
+
+function runGame(){
+
+
+    let userScore =0;
+    let computerScore =0;
+
+    for (let i =0; i <5; i++){
+        let userChoice = getUserChoice();
+        let computerChoice = getComputerChoice();
+        if (userChoice ===2){
+            break;
+        }
+        else{
+            let outcome = runRound(computerChoice, userChoice);
+            if (outcome === 2){
+                
+            } else if (outcome ===1){
+                userScore++;
+            } else {
+                computerScore++;
+            }
+        }
+        console.log(`User Score : ${userScore} || Computer Score : ${computerScore}`);
+    }
+
+    if (userScore > computerScore){
+        console.log("User Wins")
+    } else if(userScore<computerScore){
+        console.log("Computer Wins")
+    } else{
+        console.log("It is a TIE!!")
+    }
+}
+
+runGame();
